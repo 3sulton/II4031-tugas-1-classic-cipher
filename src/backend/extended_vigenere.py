287b337860ac39f882aafecf0a649f5ef11fb83d
3sulton <16520492@std.stei.itb.ac.id>
2023-02-02 15:24:09 +0700

    feat: add extended vigenere encrypt decrypt

---
+++ src/backend/extended_vigenere.py
@@ -0,0 +1,60 @@
+from chiper import chiper
+from vigenere import vigenere
+
+class extendedVigenere(vigenere):
+    def __init__(self, M="", K="", C=""):
+        super().__init__(M, K, C)
+
+    def read_bytecode(self, filename):
+        self.filename = filename
+        with open(filename, "rb") as file:
+            return bytearray(file.read())
+
+    def ascii_to_int(self, c):
+        return ord(c)
+
+    def int_to_ascii(self, n):
+        return chr(n)
+
+    def encrypt(self, filename, fileout):
+        self.byte_encrypt(filename, fileout)
+    
+    def decrypt(self, filename, fileout):
+        self.byte_decrypt(filename, fileout)
+
+    def byte_encrypt(self, filename, fileout):
+        self.C = self.read_bytecode(filename)
+        i = 0
+        j = 0
+        while (i < len(self.C)):
+            self.C[i] = chiper.e(self.C[i], chiper.char_to_int(self.K[j]), 256)
+            i += 1
+            j = (j + 1) % len(self.K)
+
+        with open(fileout, "wb") as file:
+            file.write(bytes(self.C))
+    
+    def byte_decrypt(self, filename, fileout):
+        self.M = self.read_bytecode(filename)
+        i = 0
+        j = 0
+        while (i < len(self.M)):
+            self.M[i] = chiper.d(self.M[i], chiper.char_to_int(self.K[j]), 256)
+            i += 1
+            j = (j + 1) % len(self.K)
+
+        with open(fileout, "wb") as file:
+            file.write(bytes(self.M))
+
+def test_encrypt():
+    ev = extendedVigenere(K="b")
+    ev.encrypt("../file/1.jpg", "../file/1-e.jpg")
+
+def test_decrypt():
+    ev = extendedVigenere(K="b")
+    ev.decrypt("../file/1-e.jpg", "../file/1-d.jpg")
+
+if __name__ == "__main__":
+    test_encrypt()
+    test_decrypt()
+    print("Everything passed")
