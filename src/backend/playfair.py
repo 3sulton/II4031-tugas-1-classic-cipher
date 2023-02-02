8b9b0a0726ba65d6d1c8e277afee81c35e64c5c5
3sulton <16520492@std.stei.itb.ac.id>
2023-02-02 08:52:17 +0700

    feat: playfair decryption

--- src/backend/playfair.py
+++ src/backend/playfair.py
@@ -27,6 +27,7 @@ class playfair(chiper):
                 k += 1
 
     def encrypt(self):
+        self.C = ""
         i = 0
         msg_length = len(self.M)
         while (i < msg_length):
@@ -69,7 +70,37 @@ class playfair(chiper):
                 self.C = self.C + \
                 self.matrix_key[r1][c2] + self.matrix_key[r2][c1]
 
-
+    def decrypt(self):
+        self.M = ""
+        i = 0
+        cpr_length = len(self.C)
+        while (i < cpr_length):
+            # SETUP 2 KARAKTER AWAL DULU
+            # karakter pertama
+            cp1 = self.C[i]
+            # karakter kedua
+            cp2 = self.C[i + 1]
+            i += 2
+            
+            # ALGORITMA UNTUK ENKRIP DENGAN MELIHAT KEY MATRIKS
+            # Periksa index dari tiap message
+            idx_cp1 = np.where(self.matrix_key == cp1)
+            idx_cp2 = np.where(self.matrix_key == cp2)
+            (r1, c1) = idx_cp1[0][0], idx_cp1[1][0]
+            (r2, c2) = idx_cp2[0][0], idx_cp2[1][0]
+            # jika dalam baris yang sama
+            if r1 == r2:
+                self.M = self.M + \
+                self.matrix_key[r1][(c1 - 1) % 5] + self.matrix_key[r2][(c2 - 1) % 5]
+            elif c1 == c2:
+                self.M = self.M + \
+                self.matrix_key[(r1 - 1) % 5][c1] + self.matrix_key[(r2 - 1) % 5][c2]
+            else:
+                self.M = self.M + \
+                self.matrix_key[r1][c2] + self.matrix_key[r2][c1]
+        # TO DO:
+        # menghapus nilai X yang tidak perlu
+        # huruf J pada pesan sesungguhnya akan menjadi huruf I, gimana ngatasinnya
         
 def test_key_string_to_matrix():
     pf = playfair("", "jalan ganesha sepuluh", "")
@@ -97,7 +128,7 @@ def test_encrypt():
 
     pf = playfair("ow", "jalan ganesha sepuluh", "")
     pf.encrypt()
-    assert pf.C == "WL"\
+    assert pf.C == "WL"
     
     pf = playfair("hz", "jalan ganesha sepuluh", "")
     pf.encrypt()
@@ -111,8 +142,46 @@ def test_encrypt():
     pf.encrypt()
     assert pf.C == "ZBRSFYKUPGLGRKVSNLQV"
 
+    pf = playfair("temui ibu nanti malam jo", "jalan ganesha sepuluh", "")
+    pf.encrypt()
+    assert pf.C == "ZBRSFYKUPGLGRKVSNLRCQW"
+
+def test_decrypt():
+    pf = playfair("", "jalan ganesha sepuluh", "FK")
+    pf.decrypt()
+    assert pf.M == "DI"
+
+    pf = playfair("", "jalan ganesha sepuluh", "RM")
+    pf.decrypt()
+    assert pf.M == "QT"
+
+    pf = playfair("", "jalan ganesha sepuluh", "PX")
+    pf.decrypt()
+    assert pf.M == "NQ"
+
+    pf = playfair("", "jalan ganesha sepuluh", "WL")
+    pf.decrypt()
+    assert pf.M == "OW"
+    
+    pf = playfair("", "jalan ganesha sepuluh", "BW")
+    pf.decrypt()
+    assert pf.M == "HZ"
+
+    pf = playfair("", "jalan ganesha sepuluh", "WB")
+    pf.decrypt()
+    assert pf.M == "ZH"
+
+    pf = playfair("", "jalan ganesha sepuluh", "ZBRSFYKUPGLGRKVSNLQV")
+    pf.decrypt()
+    assert pf.M == "TEMUIXIBUNANTIMALAMX"
+
+    pf = playfair("", "jalan ganesha sepuluh", "ZBRSFYKUPGLGRKVSNLRCQW")
+    pf.decrypt()
+    assert pf.M == "TEMUIXIBUNANTIMALAMIOX"
+
 
 if __name__ == "__main__":
     test_key_string_to_matrix()
     test_encrypt()
+    test_decrypt()
     print("Everything passed")
